42d5fc8617089b73ae750e495b272cf571f0c93f
blankaex <blankaex@protonmail.com>
2020-07-05 17:18:57 +1000

    updated variable name

--- dotfiles/core/.local/src/scripts/lock.sh
+++ dotfiles/core/.local/src/scripts/lock.sh
@@ -2,12 +2,12 @@
 
 . "$HOME/.cache/wal/colors.sh"
 a="ff"
-a2="80"
+b="80"
 
 i3lock --blur=5 --clock --indicator --radius=150 --ring-width=5.0 \
     --timecolor=$foreground$a --timestr='%H時%M分' --time-font='Source Han Sans JP' \
     --datecolor=$foreground$a --datestr='%m月%d日' --date-font='Source Han Sans JP' \
-    --insidecolor=$color0$a2 --insidevercolor=$color0$a2 --insidewrongcolor=$color0$a2  \
+    --insidecolor=$color0$b --insidevercolor=$color0$b --insidewrongcolor=$color0$b  \
     --ringcolor=$color6$a --ringvercolor=$color6$a --ringwrongcolor=$color3$a \
     --line-uses-inside --separatorcolor=$color6$a \
     --keyhlcolor=$color3$a --bshlcolor=$color3$a \
