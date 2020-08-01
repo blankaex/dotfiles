74c703a1020931c8410c8dce62c87156872f6072
blankaex <blankaex@protonmail.com>
2020-08-01 12:22:34 +1000

    removed fluff from output

--- dotfiles/core/.local/src/scripts/wttr.sh
+++ dotfiles/core/.local/src/scripts/wttr.sh
@@ -2,8 +2,8 @@
 
 # only update cached weather info if it's older than one day
 if [ $(stat -c '%Y' $XDG_CACHE_HOME/wttr) -lt $(date -d 'now - 1 day' +%s) ]; then
-    curl wttr.in/$WTTR_LOCATION > $XDG_CACHE_HOME/wttr
+    curl wttr.in/$WTTR_LOCATION | awk 'NR>=2&&NR<=37' > $XDG_CACHE_HOME/wttr
 fi
 
-bspc rule -a Alacritty:wttr -o monitor=DVI-I-1 state=floating follow=on center=on rectangle=1024x700+0+0
+bspc rule -a Alacritty:wttr -o monitor=DVI-I-1 state=floating follow=on center=on rectangle=1024x660+0+0
 alacritty --class wttr -e less $XDG_CACHE_HOME/wttr
