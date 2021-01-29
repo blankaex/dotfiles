810afeaf8158e270f583a37aee8a91cf944edfbc
blankaex <blankaex@protonmail.com>
2021-01-29 16:38:46 +1100

    re-implemented some scripts in ruby for cleanliness

---
+++ dotfiles/desktop/.config/sxhkd/scripts/confirm.rb
@@ -0,0 +1,3 @@
+#!/bin/ruby
+
+exec(ARGV[1]) if `printf "No\\nYes" | rofi -dmenu -i -m primary -p "#{ARGV[0]}"`.strip == "Yes"
---
+++ dotfiles/desktop/.config/sxhkd/scripts/lutris-launcher.rb
@@ -0,0 +1,13 @@
+#!/bin/ruby
+
+require "json"
+
+blob = JSON.parse(`lutris -ljo`)
+
+games = blob.map{|object| object["name"]}
+
+selection = `echo "#{games.join("\n")}" | rofi -dmenu -i -m primary -p "Game"`.strip
+
+slug = blob.detect{|object| object["name"] == selection}["slug"]
+
+exec("lutris lutris:rungame/#{slug}")
