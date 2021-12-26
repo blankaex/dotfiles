237e885471ccf5f489237114ac545d2369f29f65
blankaex <blankaex@protonmail.com>
2021-12-26 15:19:23 +1100

    added neofetch configs

---
+++ tasks/configs/neofetch.py
@@ -0,0 +1,14 @@
+from pyinfra import host
+from pyinfra.facts.server import *
+from pyinfra.operations import files
+
+
+home = host.get_fact(Home)
+hostname = host.get_fact(Hostname)
+
+
+files.rsync(
+    name="Deploy neofetch configs",
+    src="dotfiles/.config/neofetch",
+    dest=f"{home}/.config/neofetch"
+)
