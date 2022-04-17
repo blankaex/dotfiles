534c3976bbf23627817261451fd5e3fb1a4a9b5a
blankaex <blankaex@protonmail.com>
2022-04-17 23:43:58 +1000

    fixed typo

--- dotfiles/.config/sxhkd/scripts/bspwm-unhide.rb
+++ dotfiles/.config/sxhkd/scripts/bspwm-unhide.rb
@@ -5,7 +5,7 @@ count = `bspc query -N -n .hidden | wc -l`.to_i()
 if count > 0
     if count > 1
         node = `bspc query -N -n .hidden -n .sticky |
-            xargs xtitle -f "%s: %u\n"|
+            xargs xtitle -f "%s: %u\n" |
             rofi -m primary -dmenu -i -p "Unhide" |
             sed 's/.*: //' |
             xargs printf "0x%x\n"`.strip()
