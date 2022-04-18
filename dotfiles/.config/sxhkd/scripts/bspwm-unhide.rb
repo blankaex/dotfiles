67262a2582e658eaa512b3c8962c9a7e119f5ad4
blankaex <blankaex@protonmail.com>
2022-04-18 12:45:21 +1000

    the ruby way

--- dotfiles/.config/sxhkd/scripts/bspwm-unhide.rb
+++ dotfiles/.config/sxhkd/scripts/bspwm-unhide.rb
@@ -2,11 +2,13 @@
 
 count = `bspc query -N -n .hidden | wc -l`.to_i()
 
-if count == 1
-    node = `bspc query -N -n .hidden -n .sticky | tail -n1`.strip()
-elsif count > 1
-    node = `bspc query -N -n .hidden -n .sticky |
-        xargs xtitle -f "%u: %s\n" |
-        rofi -m primary -dmenu -i -p "Unhide"`.split(":")[0]
+if count > 0
+    node = case
+        when count == 1
+            `bspc query -N -n .hidden -n .sticky | tail -n1`.strip()
+        when count > 1
+            `bspc query -N -n .hidden -n .sticky | xargs xtitle -f "%u: %s\n" |
+                rofi -m primary -dmenu -i -p "Unhide"`.split(":")[0]
+    end
+    exec "bspc node #{node} -g hidden=off -g sticky=off" if node
 end
-exec "bspc node #{node} -g hidden=off -g sticky=off" if node
