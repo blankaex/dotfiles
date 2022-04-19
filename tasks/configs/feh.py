f66ffbaeb5f9ed472ebdd1426c8227334b4a0e81
blankaex <blankaex@protonmail.com>
2022-04-19 14:44:28 +0900

    updated templates

--- tasks/configs/feh.py
+++ tasks/configs/feh.py
@@ -1,16 +1,32 @@
 from pyinfra import host
 from pyinfra.facts.server import *
 from pyinfra.operations import files
+from pyinfra.operations import server
 
 
 home = host.get_fact(Home)
 hostname = host.get_fact(Hostname)
 
 
-# TODO download and set a temp wallpaper
 files.template(
     name="Deploy feh configs",
     src="dotfiles/.fehbg.j2",
     dest=f"{home}/.fehbg",
+    home=home,
     hostname=hostname
+)
+
+
+files.rsync(
+    name="Deploy temporary wallpaper",
+    src="config/wallpaper.jpg",
+    dest=f"{home}/Pictures/wallpapers/wallpaper.jpg"
+)
+
+
+server.shell(
+    name="Apply temporary wallpaper",
+    commands=[
+        f"feh --bg-fill {home}/Pictures/wallpapers/wallpaper.jpg"
+    ]
 )
