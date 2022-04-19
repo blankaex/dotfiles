50cc12af2bffbab5a554c06e46f2a6d745ef785a
blankaex <blankaex@protonmail.com>
2022-04-19 15:56:46 +1000

    added startpage task

---
+++ tasks/packages/startpage.py
@@ -0,0 +1,32 @@
+from pyinfra import host
+from pyinfra.operations import git
+from pyinfra.operations import systemd
+
+
+home = host.get_fact(Home)
+
+
+git.repo(
+    name="Clone startpage",
+    src="https://github.com/blankaex/devola",
+    dest=f"{home}/.local/src/devola"
+)
+
+
+files.template(
+    name="Deploy startpage service",
+    src="root/etc/systemd/system/startpage.service.j2",
+    dest=f"/etc/systemd/system/startpage.service",
+    sudo=True,
+    home=home,
+    colors=colors
+)
+
+
+systemd.service(
+    name="Enable startpage service",
+    service="startpage",
+    running=True,
+    enabled=True,
+    user_mode=True
+)
