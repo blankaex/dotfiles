52565b5d86daacc3dd9b5dc731581cbd65d72206
blankaex <blankaex@protonmail.com>
2022-04-25 17:17:31 +0900

    fixed volume & brightness scripts

--- dotfiles/.config/sxhkd/scripts/brightness.rb
+++ dotfiles/.config/sxhkd/scripts/brightness.rb
@@ -1,5 +1,14 @@
 #!/usr/bin/env ruby 
 
-brightness = `printf "0.5\\n1.0" | rofi -dmenu -i -m primary -p "Brightness"`.strip
-system "xrandr --output DVI-D-0 --brightness #{brightness}"
-system "xrandr --output DVI-I-1 --brightness #{brightness}"
+if ARGV.length == 2 and ["-u", "-d"].include? ARGV[0] and ARGV[1].to_i > 0
+  # clean this up
+  brightness = `brightnessctl`.split("\n")[1].split("(")[1].split("%")[0].to_i
+  value = case
+      when ARGV[0] == "-u"
+        brightness + ARGV[1].to_i
+      when ARGV[0] == "-d"
+        brightness - ARGV[1].to_i
+      end
+  new = `brightnessctl s #{value}%`.split("\n")[2].split("(")[1].split("%")[0]
+  exec "dunstify -r 102 '明るさ: #{new}'"
+end
