8718531ab727a278a68ca4a2356dc22843786766
blankaex <blankaex@protonmail.com>
2022-05-21 13:25:02 +0900

    updated mpd config & install

--- tasks/packages/mpd.py
+++ tasks/packages/mpd.py
@@ -2,13 +2,31 @@ from pyinfra.operations import pacman
 
 
 pacman.packages(
-    name="Install mpd + ncmpcpp",
+    name="Install mpd + dependencies",
     packages=[
         "mpd",
+        "mpc",
         "ncmpcpp",
-        "ueberzug"
+        "ueberzug",
+        "inotify-tools"
     ],
     update=True,
     upgrade=True,
     sudo=True
+)
+
+
+systemd.service(
+    name="Enable mpd",
+    service="mpd",
+    running=True,
+    enabled=True,
+    user_mode=True
+)
+
+
+files.rsync(
+    name="Touch cover image",
+    src="dotfiles/.config/ncmpcpp/cover.png",
+    dest=f"{home}/.cache/cover.png"
 )
