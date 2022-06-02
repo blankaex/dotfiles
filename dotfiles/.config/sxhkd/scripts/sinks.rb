8bb1cdc88a21a2cb0088ed62decf5d8ccaa01fa1
blankaex <blankaex@protonmail.com>
2022-06-02 19:42:07 +0900

    fixed prompt

--- dotfiles/.config/sxhkd/scripts/sinks.rb
+++ dotfiles/.config/sxhkd/scripts/sinks.rb
@@ -2,7 +2,7 @@
 
 output = `pactl list sinks`.split("\n")
 sinks = output.select{|line| line.include? "Name: "}.map{|line| line.split(": ")[1]}
-selection = `echo "#{sinks.join("\n")}" | rofi -dmenu -i -m primary -p "#{`pactl get-default-sink`}"`.strip
+selection = `echo "#{sinks.join("\n")}" | rofi -dmenu -i -m primary -p "Sink (#{`pactl get-default-sink`.strip})"`.strip
 
 if !selection.empty? and sinks.include? selection
     exec "pactl set-default-sink #{selection}"
--- dotfiles/.config/sxhkd/scripts/sources.rb
+++ dotfiles/.config/sxhkd/scripts/sources.rb
@@ -2,7 +2,7 @@
 
 output = `pactl list sources`.split("\n")
 sinks = output.select{|line| line.include? "Name: "}.map{|line| line.split(": ")[1]}
-selection = `echo "#{sinks.join("\n")}" | rofi -dmenu -i -m primary -p "#{`pactl get-default-source`}"`.strip
+selection = `echo "#{sinks.join("\n")}" | rofi -dmenu -i -m primary -p "Source (#{`pactl get-default-source`.strip})"`.strip
 
 if !selection.empty? and sinks.include? selection
     exec "pactl set-default-source #{selection}"
