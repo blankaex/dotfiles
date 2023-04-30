bf99391705a803a3d5a5e0e54b281a041fe8044b
blankaex <blankaex@protonmail.com>
2023-04-30 15:00:46 +0900

    added bluetooth disconnect & error handling

--- dotfiles/.config/sxhkd/scripts/bluetooth.rb
+++ dotfiles/.config/sxhkd/scripts/bluetooth.rb
@@ -1,10 +1,14 @@
 #!/usr/bin/env ruby 
 
-devices = {}
-for line in `bluetoothctl devices`.split("\n")
-  device = line.split(" ", 3)
-  devices[device[2]] = device[1]
+unless ARGV.empty?
+  devices = {}
+  for line in `bluetoothctl devices`.split("\n")
+    device = line.split(" ", 3)
+    devices[device[2]] = device[1]
+  end
+  selection = `echo "#{devices.keys.join("\n")}" | rofi -dmenu -i -m primary -p "#{ARGV[0].capitalize}"`.strip
+  unless selection.empty?
+    output = `bluetoothctl #{ARGV[0]} #{devices[selection]}`
+    exec "dunstify 'bluetoothctl' '#{output}'"
+  end
 end
-selection = `echo "#{devices.keys.join("\n")}" | rofi -dmenu -i -m primary -p "Device"`.strip
-output = `bluetoothctl connect #{devices[selection]}`
-exec "dunstify 'bluetoothctl' '#{output}'"
