54551fd060b6716d8199bc164c2b01b853d48d5f
blankaex <blankaex@protonmail.com>
2024-07-05 23:04:23 +0900

    re-enabled notification pause

--- dotfiles/.config/sxhkd/scripts/notify-pause.rb
+++ dotfiles/.config/sxhkd/scripts/notify-pause.rb
@@ -3,15 +3,22 @@
 options = [
     30,
     60,
-    "Unpause"
+    120
 ]
+
+if `dunstctl is-paused`.strip() == "true"
+    options.append("Unpause")
+end
 
 selection = `printf "#{options.join("\\n")}" | rofi -dmenu -i -m primary -p "Pause"`.strip()
 
 if selection.to_i > 0
+    system "dunstify 'Notifications Paused' 'Notifications paused for #{selection.to_i} minutes.'"
+    sleep(3)
     system "dunstctl set-paused true"
     sleep(selection.to_i * 60)
     exec "dunstctl set-paused false"
 elsif selection == "Unpause"
+    system 'dunstify "Notifications Unpaused" "Notifications unpaused."'
     exec "dunstctl set-paused false"
 end
