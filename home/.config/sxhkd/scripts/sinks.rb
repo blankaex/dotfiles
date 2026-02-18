92997f45ac3b2e80c9efc1e6b074ce90d3b40124
blankaex <blankaex@protonmail.com>
2026-02-18 23:38:38 +0900

    removed configs not needed for small install

--- home/.config/sxhkd/scripts/sinks.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby 
-
-output = `pactl list sinks`.split("\n")
-sinks = output.select{|line| line.include? "Name: "}.map{|line| line.split(": ")[1]}
-selection = `echo "#{sinks.join("\n")}" | rofi -dmenu -i -m primary -p "Sink (#{`pactl get-default-sink`.strip})"`.strip
-
-if !selection.empty? and sinks.include? selection
-    exec "pactl set-default-sink #{selection}"
-end
