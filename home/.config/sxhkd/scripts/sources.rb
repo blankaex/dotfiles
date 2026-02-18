db224f7e7d34f05bde7e2d1dc255bf34fedc6285
blankaex <blankaex@protonmail.com>
2026-02-18 23:38:38 +0900

    removed configs not needed for small install

--- home/.config/sxhkd/scripts/sources.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby 
-
-output = `pactl list sources`.split("\n")
-sinks = output.select{|line| line.include? "Name: "}.map{|line| line.split(": ")[1]}
-selection = `echo "#{sinks.join("\n")}" | rofi -dmenu -i -m primary -p "Source (#{`pactl get-default-source`.strip})"`.strip
-
-if !selection.empty? and sinks.include? selection
-    exec "pactl set-default-source #{selection}"
-end
