7fb60744d4a8d4cca090cf68a429bbe04a464479
blankaex <blankaex@protonmail.com>
2026-02-18 23:38:38 +0900

    removed configs not needed for small install

--- home/.config/dunst/notify-sound.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-case $1 in
-"RuneLite")
-    paplay "/media/storage/audio/Theme Sounds/Misc/long-pop.wav"
-    ;;
-esac
--- home/.config/sxhkd/scripts/confirm.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env ruby
-
-exec(ARGV[1]) if `printf "No\\nYes" | rofi -dmenu -i -m primary -p "#{ARGV[0]}"`.strip() == "Yes"
